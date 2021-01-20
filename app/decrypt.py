2e69eb3ebf49e0f7ce7c06822ae169648b8dc930
TomHolroyd <tomholroyd96@gmail.com>
2021-01-20 14:39:56 +0000

    Reverting back to .txt file

--- app/decrypt.py
+++ app/decrypt.py
@@ -11,10 +11,13 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 
 def decrypt_comment(comment_token: str) -> dict:
-
     key_byte = Path('keys/comment_key').read_bytes()
     comment_key = base64.b64encode(key_byte)
+    print(comment_key)
+    logger.info(comment_key)
 
-    f = Fernet(comment_key)
+    logger.info("decrypting survey")
+    key = Path('comment_key.txt').read_bytes()
+    f = Fernet(key)
     comment_bytes = f.decrypt(comment_token.encode())
     return json.loads(comment_bytes.decode())
