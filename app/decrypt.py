011f61076f8cea3532fedbbc60f88c543cce66f8
TomHolroyd <tomholroyd96@gmail.com>
2021-01-20 13:47:26 +0000

    comment key now accessd through k8 mount

--- app/decrypt.py
+++ app/decrypt.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import logging
 from pathlib import Path
@@ -11,7 +12,10 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 def decrypt_comment(comment_token: str) -> dict:
     logger.info("decrypting survey")
-    key = Path('comment_key.txt').read_bytes()
+
+    key_byte = Path('app/comment_key').read_bytes()
+    key = base64.b64encode(key_byte)
+
     f = Fernet(key)
     comment_bytes = f.decrypt(comment_token.encode())
     return json.loads(comment_bytes.decode())
