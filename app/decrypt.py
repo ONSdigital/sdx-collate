fa4fe5fa4f29a7ad1e50956da1529305e65c2797
TomHolroyd <tomholroyd96@gmail.com>
2021-01-20 17:03:04 +0000

    Reverting back to DNS service discover and txt file

--- app/decrypt.py
+++ app/decrypt.py
@@ -11,13 +11,12 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 
 def decrypt_comment(comment_token: str) -> dict:
-    key_byte = Path('keys/comment_key').read_bytes()
-    comment_key = base64.b64encode(key_byte)
+    # key_byte = Path('keys/comment_key').read_bytes()
+    # comment_key = base64.b64encode(key_byte)
 
-    # logger.info("decrypting survey")
-    # key = Path('../comment_key.txt').read_bytes()
-
-    f = Fernet(comment_key)
+    logger.info("decrypting survey")
+    key = Path('../comment_key.txt').read_bytes()
+    f = Fernet(key)
 
     comment_bytes = f.decrypt(comment_token.encode())
     return json.loads(comment_bytes.decode())
