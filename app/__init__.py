0464145c903627f54dfb3887d67f69a8b453770e
TomHolroyd <tomholroyd96@gmail.com>
2021-02-16 12:22:26 +0000

    Adding makefile

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,8 @@ PROJECT_ID = os.getenv('PROJECT_ID', 'ons-sdx-sandbox')
 DELIVER_SERVICE_URL = "sdx-deliver:80"
 
 DECRYPT_COMMENT_KEY = get_secret(PROJECT_ID, 'sdx-comment-key')
+
+
+def get_test_config():
+    global DECRYPT_COMMENT_KEY
+    DECRYPT_COMMENT_KEY = "E3rjFT2i9ALcvc99Pe3YqjIGrzm3LdMsCXc8nUaOEbc="
