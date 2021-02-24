9607abe80ab55397e90171dd62e5b6b6d6b4415a
TomHolroyd <tomholroyd96@gmail.com>
2021-02-24 11:47:11 +0000

    Config changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+from google.cloud import datastore
+
 from app.logger import logging_config
 from app.secret_manager import get_secret
 
@@ -14,10 +16,13 @@ class Config:
     def __init__(self, proj_id) -> None:
         self.PROJECT_ID = proj_id
         self.DECRYPT_COMMENT_KEY = "E3rjFT2i9ALcvc99Pe3YqjIGrzm3LdMsCXc8nUaOEbc="
+        self.DATASTORE_CLIENT = None
 
 
 CONFIG = Config(project_id)
 
 
 def cloud_config():
+    datastore_client = datastore.Client(project=CONFIG.PROJECT_ID)
+    CONFIG.DATASTORE_CLIENT = datastore_client
     CONFIG.DECRYPT_COMMENT_KEY = get_secret(CONFIG.PROJECT_ID, 'sdx-comment-key')
