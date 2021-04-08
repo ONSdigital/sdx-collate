232a124a64f28fb83de8abbcab69628542503e98
Jon Kent <jonkent95@gmail.com>
2021-04-08 11:08:40 +0100

    test fetch comments

--- tests/test_datastore_connect.py
+++ tests/test_datastore_connect.py
@@ -1,8 +1,7 @@
 import unittest
 
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from app.datastore_connect import fetch_comments
-from app import CONFIG
 
 
 class TestDataStoreConnect(unittest.TestCase):
@@ -10,19 +9,14 @@ class TestDataStoreConnect(unittest.TestCase):
     @patch('app.datastore_connect.CONFIG')
     @patch('app.datastore_connect.decrypt_comment')
     def test_fetch_comments(self, mock_decrypt_comment, mock_config):
-        mock_datastore_client_setup = mock_config.datastore.Client
-        mock_datastore_client = mock_datastore_client_setup(project=CONFIG.PROJECT_ID)
-        mock_query = mock_datastore_client.query
-        mock_fetch = mock_query.fetch.return_value = {
-                'Comment': '032dbc9b-ef8c-46cb-b657-8672d3a57320',
+        mock_query = Mock()
+        mock_query.fetch.return_value = [{
+                'Comment': 'encrypted comment',
                 'encrypted_data': 'value',
                 'period': '201817',
-                'survey': '019'
-            }
-        fetch_comments()
-        mock_fetch.assert_called()
-        mock_query.assert_called_with(kind='Comment')
-        mock_decrypt_comment.assert_called()
+                'survey_id': '019'
+            }]
+        mock_config.DATASTORE_CLIENT.query.return_value = mock_query
+        mock_decrypt_comment.return_value = "decrypted comment"
 
-
-
+        self.assertEqual({'019_201817': ['decrypted comment']}, fetch_comments())
