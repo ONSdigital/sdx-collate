8bed5371c784088478070e2e156159563729976f
Jon Kent <jonkent95@gmail.com>
2022-03-21 10:22:25 +0000

    pass in day

--- run_daily.py
+++ run_daily.py
@@ -7,7 +7,7 @@ from app.collate import create_daily_zip_only, create_full_zip
 logger = structlog.get_logger()
 
 # change to the desired date before running
-chosen_day = date(2022, 3, 17)
+chosen_day = date(2022, 3, 22)
 
 # create full zip or just daily
 full_zip = True
