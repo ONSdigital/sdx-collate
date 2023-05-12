08c01941071c9fbbc738c2682392d8ea01949fe1
Jon Kent <jonkent95@gmail.com>
2023-05-12 15:57:17 +0100

    Test requirements

--- app/deliver.py
+++ app/deliver.py
@@ -13,5 +13,5 @@ def deliver_comments(file_name: str, zip_file: IO[bytes]):
     Returns True or raises appropriate error on response.
     """
     domain = CONFIG.DELIVER_SERVICE_URL
-    endpoint = "/deliver/comments"
+    endpoint = "deliver/comments"
     post(domain, endpoint, None, params={"filename": file_name}, files={DELIVER_NAME: zip_file})
